9d97a907b0e886040fa04a43c72cec7be554e32c
Elizabeth Peters <eliznyc@gmail.com>
2019-08-30 02:23:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function takeANumber(katzDeliLine, name) {
     katzDeliLine.push(name);
-    return ("Welcome, " + name + ". You are number " + (katzDeliLine.length) + " in line.")
+    return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
 }
 
 function nowServing(katzDeliLine) {
-  katzDeliLine.shift();
+  if {katzDeliLine.length===0} return ("There is nobody waiting to be served!") else {
+  katzDeliLine.shift()}
 }
