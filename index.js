0613694066e400e4e6f92f4adcf7869fab3903f3
Elizabeth Peters <eliznyc@gmail.com>
2019-08-30 02:15:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-
-
 function takeANumber(katzDeliLine, name) {
     katzDeliLine.push(name);
     return ("Welcome, " + name + ". You are number " + (katzDeliLine.length) + " in line.")
+}
+
+function nowServing(katzDeliLine) {
+  katzDeliLine.shift();
 }
