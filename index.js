8ecc0b584a40fec0a7ddaf92b6d79577e2f0b177
Elizabeth Peters <eliznyc@gmail.com>
2019-08-30 02:29:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length===0) {return ("There is nobody waiting to be served!")} else {
+  if (katzDeliLine.length===0) {return ("There is nobody waiting to be served!")} else { return (`Currently serving ${katzDeliLine[0]}.`)
   katzDeliLine.shift(0)}
 }
